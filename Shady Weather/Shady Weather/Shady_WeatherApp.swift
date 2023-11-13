316156487dc52ab2728b5e722fe81346065672ef
Aleksei Iakushev <av.yakush@gmail.com>
2023-11-13 09:56:47 +0300

    Xcode project created

---
+++ Shady Weather/Shady Weather/Shady_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shady_WeatherApp.swift
+//  Shady Weather
+//
+//  Created by Aleksey Yakushev on 13.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct Shady_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
